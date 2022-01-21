70f012b6761476574ff02012836b6d41bcae2a43
FluffyCat20 <71210269+FluffyCat20@users.noreply.github.com>
2022-01-21 17:53:19 +0300

    fix: failed merge

--- Lax_Friedrichs_method/main_1d.cpp
+++ Lax_Friedrichs_method/main_1d.cpp
@@ -7,6 +7,7 @@ int main(){
   std::ifstream input("D:/gas_dyn/idk/Lax_Friedrichs_method/config.json");
   data grid(input);
   input.close();
+  grid.delta_t = grid.calc_delta_t();
 
   int method_number = -1;
   if (grid.method_name == "lax_friedrichs")
@@ -18,7 +19,7 @@ int main(){
   if (grid.method_name == "godunov")
     method_number = 3;
 
-  if (method_number == -1) {
+  if (method_number == -1){
     std::cout << "Error: unknown method name" << std::endl;
     return -1;
   }
@@ -52,7 +53,7 @@ int main(){
 
       case 2:
       new_grid.delta_t = grid.calc_delta_t();
-      if (current_t + new_grid.delta_t > new_grid.t_end) {
+      if (current_t + new_grid.delta_t > new_grid.t_end){
         new_grid.delta_t = new_grid.t_end - current_t;
       }
       new_grid.calc_new_U_with_mac_cormack_davis(grid);
@@ -81,49 +82,17 @@ int main(){
   std::cout << current_t << " steps: " << counter << std::endl;
 
   //output
-  if (grid.dimension == 1 && grid.method_name != "mac_cormack") {
-    std::ofstream fout("out_" + grid.method_name + std::to_string(new_grid.size_x) + ".dat");
-    fout << "x     rho     p     u\n";
-    for (size_t i = 0; i < new_grid.size_x; ++i) {
-      fout << i*new_grid.delta_x << " "
-           << new_grid.mesh_1D[i].rho << " "
-           << new_grid.mesh_1D[i].p << " "
-           << new_grid.mesh_1D[i].u[0] << "\n";
-    }
-    fout.close();
+  std::ofstream fout("out_" + grid.method_name + std::to_string(new_grid.size) + ".txt");
+  fout << "x     rho     p     u\n";
+  for (size_t i = 0; i < new_grid.size; ++i){
+    fout << i*new_grid.delta_x << " "
+         << new_grid.mesh[i].rho << " "
+         << new_grid.mesh[i].p << " "
+         << new_grid.mesh[i].u << "\n";
   }
+  fout.close();
 
-  if (grid.dimension == 1 && grid.method_name == "mac_cormack") {
-    std::ofstream fout("out_" + grid.method_name + std::to_string(new_grid.size_x) + ".dat");
-    fout << "x     rho     p     u\n";
-    for (size_t i = 0; i < new_grid.size_x; ++i) {
-      fout << i*new_grid.delta_x << " "
-           << new_grid.mesh[1][i].rho << " "
-           << new_grid.mesh[1][i].p << " "
-           << new_grid.mesh[1][i].u[0] << "\n";
-    }
-    fout.close();
-  }
-
-  if (grid.dimension == 2 && grid.method_name == "mac_cormack") {
-    std::ofstream fout("out_" + grid.method_name + std::to_string(new_grid.size_x)
-                       + 'x' + std::to_string(new_grid.size_y)+ ".dat");
-    fout << "x     y     rho     p     u     v\n";
-    for (size_t i = 0; i < new_grid.size_y; ++i) {
-      for (size_t j = 0; j < new_grid.size_x; ++j) {
-        fout << j*new_grid.delta_x << " "
-             << i*new_grid.delta_y << " "
-             << new_grid.mesh[i][j].rho << " "
-             << new_grid.mesh[i][j].p << " "
-             << new_grid.mesh[i][j].u[0] << " "
-             << new_grid.mesh[i][j].u[1] << "\n";
-      }
-    }
-    fout.close();
-  }
-
-  std::ofstream fout;
-  fout.open("out_exact_solution.dat");
+  fout.open("out_exact_solution.txt");
   exact_solution exact_result(grid);
   exact_result.calc_and_print_result(fout);
   fout.close();
